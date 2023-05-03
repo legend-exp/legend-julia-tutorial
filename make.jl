3fc293bb2717e264c190102cf549da5317abbd63
Oliver Schulz <oschulz@mpp.mpg.de>
2023-05-03 11:14:04 +0200

    Improve make script

--- make.jl
+++ make.jl
@@ -1,6 +1,23 @@
-# This script is licensed under the MIT License (MIT).
+# This software is licensed under the MIT "Expat" License.
 
 import Pkg
+
+# Ensure IJulia is installed in default Julia environment
+
+Pkg.activate() # Activate default environment
+Pkg.instantiate() # Need to run this only once
+
+if !("IJulia" in keys(Pkg.project().dependencies))
+    @info "Installing IJulia into default Julia environment \"$(Pkg.project().path)\""
+    Pkg.add("IJulia")
+    Pkg.build("IJulia")
+    # Should not be necessary:
+    # IJulia.installkernel("Julia", "--project=@.")
+end
+import IJulia
+
+
+# Activate tutorial environment and generate Jupyter notebook
 
 Pkg.activate(@__DIR__)
 Pkg.instantiate() # Need to run this only once
